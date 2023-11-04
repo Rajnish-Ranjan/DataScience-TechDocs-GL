407c5b22a41f15e7139d04c0b85afee4241a5f1f
Rajnish-Ranjan <rajnish.15je1619@agp.ism.ac.in>
2023-11-04 11:10:56 +0530

    adding email template task

--- image_to_docs/Imagetodocs_api_example_call.py
+++ image_to_docs/Imagetodocs_api_example_call.py
@@ -2,8 +2,8 @@ import requests
 
 url = "http://127.0.0.1:8081/process_image"
 
-files = {'image': ('vicode.jpeg', open('vicode.jpeg', 'rb'), 'image/png')}
-data = {'output_file': 'certis.docx'}
+files = {'image': ('text.png', open('text.png', 'rb'), 'image/png')}
+data = {'output_file': 'text.docx'}
 
 response = requests.post(url, files=files, data=data)
 
